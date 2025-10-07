5cff60df23f7cd4ed22ce0d24b75db9c04cf8332
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-10-07 08:53:00 -0300

    configurando css do app com container e content

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et facilis
-        distinctio recusandae est fugiat consectetur, tenetur beatae, pariatur
-        necessitatibus totam exercitationem quam tempora. Adipisci quod sunt ut
-        atque aperiam voluptatibus!
-      </p>
-      ;
+      <div className="container">
+        <div className="content">
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem dicta repudiandae quos nemo ratione velit maxime
+            asperiores sed distinctio commodi, quasi praesentium nesciunt quas
+            soluta nulla sit in nisi culpa.
+          </section>
+        </div>
+      </div>
     </>
   );
 }
